9cf8e319f495e4e7766f225943ced52adb0729dc
fpem3309 <fpem3309@gmail.com>
2025-08-29 16:42:18 +0900

    JPA와 DB설정, 동작확인

---
+++ src/test/java/jpabook/jpashop/Repository/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.Repository;
+
+import jpabook.jpashop.Entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void testMember() {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); // ⭐️같은 영속성 컨택스트의 같은 식별자면 같은 엔티티
+    }
+}
