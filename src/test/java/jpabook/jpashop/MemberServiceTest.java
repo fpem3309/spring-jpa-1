fba274c39cd5785e396e7559baec1bfbe2d010d1
fpem3309 <fpem3309@gmail.com>
2025-09-01 15:45:29 +0900

    [JPA 활용1]회원 도메인 개발 - 회원 기능 테스트

---
+++ src/test/java/jpabook/jpashop/MemberServiceTest.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("memberA");
+
+        //when
+        Long savedId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberService.findOne(savedId));
+    }
+
+    @Test
+    void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("member");
+
+        Member member2 = new Member();
+        member2.setName("member");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        //IllegalStateException 예외가 발생하지 않으면 테스트 실패
+        assertThrows(IllegalStateException.class, () ->
+                memberService.join(member2));
+
+//        fail("예외가 발생해야한다"); // ❗여기가 실행되면 테스트코드를 잘못 짬
+    }
+}
